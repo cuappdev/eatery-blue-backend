5a915b0c08ea02ae66d8510df159efee579aac65
Fanhao Yu <fy229@cornell.edu>
2025-10-05 12:39:52 -0400

    Deserialize allergen info and dietary restrictions as well as remove from create function

--- src/item/serializers.py
+++ src/item/serializers.py
@@ -5,38 +5,9 @@ from item.models import Item, DietaryPreference, Allergen
 class ItemSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(default="Item")
-    dietary_preferences = serializers.SerializerMethodField()
-    allergens = serializers.SerializerMethodField()
-
-    def get_dietary_preferences(self, obj):
-        """Get list of dietary preference names"""
-        return [pref.name for pref in obj.dietary_preferences.all()]
-    
-    def get_allergens(self, obj):
-        """Get list of allergen names"""
-        return [allergen.name for allergen in obj.allergens.all()]
 
     def create(self, validated_data):
-        dietary_prefs = validated_data.pop('dietary_preferences', [])
-        allergens = validated_data.pop('allergens', [])
         item, _ = Item.objects.get_or_create(**validated_data)
-
-        dietary_prefs_objects = []
-        for pref_name in dietary_prefs:
-            pref, _ = DietaryPreference.objects.get_or_create(name=pref_name)
-            dietary_prefs_objects.append(pref)
-        
-        if dietary_prefs_objects:
-            item.dietary_preferences.add(*dietary_prefs_objects)
-
-        allergens_objects = []
-        for allergen_name in allergens:
-            allergen, _ = Allergen.objects.get_or_create(name=allergen_name)
-            allergens_objects.append(allergen)
-
-        if allergens_objects:
-            item.allergens.add(*allergens_objects)
-
         return item
     
     def update(self, instance, validated_data):
@@ -47,11 +18,11 @@ class ItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Item
-        fields = ["id", "category", "name", "base_price", "dietary_preferences", "allergens"]
+        fields = ["id", "category", "name", "base_price"]
         read_only_fields = ["id"]
 
 
 class ItemSerializerOptimized(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ["id", "name", "dietary_preferences", "allergens"]
+        fields = ["id", "name"]
