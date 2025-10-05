03b00a70606705cf82f5498c181434be25c5d328
skyeslattery <dss354@cornell.edu>
2025-10-05 12:37:58 -0400

    reset settings

--- src/eatery_blue_backend/settings.py
+++ src/eatery_blue_backend/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # IS_PROD can be None
-DEBUG = True
+DEBUG = False if os.environ.get("IS_PROD") is True else True
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
