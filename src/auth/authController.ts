332c463f3831ca295985cc3fb7fdb0937c4e36e1
Aayush Agnihotri <aa2328@cornell.edu>
2025-11-19 23:30:43 -0500

    Adding auth service

--- src/auth/authController.ts
+++ src/auth/authController.ts
@@ -1,63 +1,15 @@
-import crypto from 'crypto';
-import jwt from 'jsonwebtoken';
-
 import type { Request, Response } from 'express';
 
-import { prisma } from '../prisma.js';
-import { ForbiddenError } from '../utils/AppError.js';
+import * as authService from './authService.js';
 
 export const verifyDeviceUuid = async (req: Request, res: Response) => {
   const { deviceUuid } = req.body;
-  const refreshToken = crypto.randomBytes(64).toString('hex');
-
-  const user = await prisma.user.upsert({
-    where: { deviceUuid },
-    // If the user exists, update the refreshToken
-    update: {
-      refreshToken,
-    },
-    // If the user does not exist, create a new user
-    create: {
-      deviceUuid,
-      refreshToken,
-    },
-  });
-
-  const accessToken = jwt.sign(
-    { userId: user.id },
-    process.env.ACCESS_TOKEN_SECRET!,
-    { expiresIn: '15m' },
-  );
-
-  return res.json({ accessToken, refreshToken });
+  const tokens = await authService.verifyDeviceUuid(deviceUuid);
+  return res.json(tokens);
 };
 
 export const refreshAccessToken = async (req: Request, res: Response) => {
   const { refreshToken } = req.body;
-
-  const user = await prisma.user.findFirst({
-    where: { refreshToken },
-  });
-
-  if (!user) {
-    throw new ForbiddenError('Invalid refresh token');
-  }
-
-  const newAccessToken = jwt.sign(
-    { userId: user.id },
-    process.env.ACCESS_TOKEN_SECRET!,
-    { expiresIn: '15m' },
-  );
-
-  const newRefreshToken = crypto.randomBytes(64).toString('hex');
-
-  await prisma.user.update({
-    where: { id: user.id },
-    data: { refreshToken: newRefreshToken },
-  });
-
-  return res.json({
-    accessToken: newAccessToken,
-    refreshToken: newRefreshToken,
-  });
+  const tokens = await authService.refreshAccessToken(refreshToken);
+  return res.json(tokens);
 };
